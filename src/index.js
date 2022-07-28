944e280e6fb2fa0940f9b946b466142a96b1c2fc
anijit <anijitsau@gmail.com>
2022-07-28 15:44:35 +0530

    favicon adding is done

--- src/index.js
+++ src/index.js
@@ -3,4 +3,7 @@ import { createRoot } from "react-dom/client";
 // components
 import App from "./App";
 
+// favicon
+import "./icons/favicon.ico";
+
 createRoot(document.getElementById("root")).render(<App />);
